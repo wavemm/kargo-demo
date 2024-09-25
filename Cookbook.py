5141a0d164cd130791f20acce0b52d75cbd18d92
Kargo Render <kargo-render@akuity.io>
2024-09-25 20:27:56 +0000

    updated stages/uat/kustomization.yaml to use image docker.io/okrehel/minirepo:0.0.1

---
+++ Cookbook.py
@@ -0,0 +1,31 @@
+REPO_URL = "https://github.com/wavemm/kargo-demo"
+
+
+def kubectl(cmd: str) -> str:
+    return f"kubectl --context kind-kargo-quickstart {cmd}"
+
+
+def kargo_create_project(recipe):
+    return [
+        f"export GITOPS_REPO_URL={REPO_URL}",
+        "export GITHUB_USERNAME=abo-abo",
+        "export GITHUB_PAT=$(gpg -q --for-your-eyes-only --no-tty -d ~/.password-store/com/github.com/kargo-research.gpg)",
+        "export DOCKER_USERNAME=okrehel",
+        "export DOCKER_PASSWORD=$(gpg -q --for-your-eyes-only --no-tty -d ~/.password-store/com/hub.docker.com/okrehel.gpg)",
+        "cd ops",
+        "kargo login https://localhost:31444 --admin --password admin --insecure-skip-tls-verify",
+        "envsubst < project.yaml | kargo apply -f -"]
+
+
+def setup(recipe):
+    return [
+        "# kind delete cluster -n kargo-quickstart",
+        "cd ops",
+        "./setup-kind-cluster.sh",
+        kubectl("apply -f argocd-kargo-demo.yaml"),
+        kubectl("-n kargo-demo-test create secret generic regcred  --from-file=.dockerconfigjson=$HOME/.docker/config.json --type=kubernetes.io/dockerconfigjson")
+    ]
+
+
+def curl_test(recipe):
+    return "curl localhost:30081"
