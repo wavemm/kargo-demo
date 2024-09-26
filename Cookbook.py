9eb8aa8a491f96db32dd649e3870a46ecf98919d
Kargo Render <kargo-render@akuity.io>
2024-09-26 09:04:16 +0000

    updated stages/test/kustomization.yaml to use image docker.io/okrehel/minirepo:2409.26.1

--- Cookbook.py
+++ Cookbook.py
@@ -14,7 +14,8 @@ def kargo_create_project(recipe):
         "export DOCKER_PASSWORD=$(gpg -q --for-your-eyes-only --no-tty -d ~/.password-store/com/hub.docker.com/okrehel.gpg)",
         "cd ops",
         "kargo login https://localhost:31444 --admin --password admin --insecure-skip-tls-verify",
-        "envsubst < project.yaml | kargo apply -f -"]
+        "envsubst < project.yaml | kargo apply -f -",
+    ]
 
 
 def setup(recipe):
@@ -23,9 +24,31 @@ def setup(recipe):
         "cd ops",
         "./setup-kind-cluster.sh",
         kubectl("apply -f argocd-kargo-demo.yaml"),
-        kubectl("-n kargo-demo-test create secret generic regcred  --from-file=.dockerconfigjson=$HOME/.docker/config.json --type=kubernetes.io/dockerconfigjson")
+        kubectl(
+            "-n kargo-demo-test create secret generic regcred  --from-file=.dockerconfigjson=$HOME/.docker/config.json --type=kubernetes.io/dockerconfigjson"
+        ),
     ]
 
 
 def curl_test(recipe):
     return "curl localhost:30081"
+
+
+def curl_uat(recipe):
+    return "curl localhost:30082"
+
+
+def curl_prod(recipe):
+    return "curl localhost:30083"
+
+
+def get_freight(recipe):
+    return "kargo get freight --project kargo-demo"
+
+
+def get_promotions(recipe):
+    return "kargo get promotions --project kargo-demo"
+
+
+def get_stages(recipe):
+    return "kargo get stages --project kargo-demo"
