16024956184488d9ca3725efebb323f0bdba4009
Kargo Render <kargo-render@akuity.io>
2024-10-01 19:17:03 +0000

    updated stages/canary/kustomization.yaml to use image docker.io/okrehel/minirepo:2410.1.3

--- Cookbook.py
+++ Cookbook.py
@@ -15,26 +15,29 @@ def kargo_create_project(recipe):
         "cd ops",
         "kargo login https://localhost:31444 --admin --password admin --insecure-skip-tls-verify",
         "envsubst < project.yaml | kargo apply -f -",
-        kubectl(
-            "-n kargo-demo-migrate create secret generic regcred  --from-file=.dockerconfigjson=$HOME/.docker/config.json --type=kubernetes.io/dockerconfigjson"
-        )
+
     ]
+
+def create_regcred(recipe):
+    return kubectl(
+        "-n kargo-demo-migrate create secret generic regcred  --from-file=.dockerconfigjson=$HOME/.docker/config.json --type=kubernetes.io/dockerconfigjson"
+    )
 
 
 def setup(recipe):
     return [
-        "# kind delete cluster -n kargo-quickstart",
+        "kind delete cluster -n kargo-quickstart",
         "cd ops",
         "./setup-kind-cluster.sh",
         kubectl("apply -f argocd-kargo-demo.yaml"),
     ]
 
 
-def curl_test(recipe):
+def curl_migrate(recipe):
     return "curl localhost:30081"
 
 
-def curl_uat(recipe):
+def curl_canary(recipe):
     return "curl localhost:30082"
 
 
@@ -52,3 +55,15 @@ def get_promotions(recipe):
 
 def get_stages(recipe):
     return "kargo get stages --project kargo-demo"
+
+
+def get_analysisruns(recipe):
+    return kubectl("get analysisruns --all-namespaces --sort-by=.metadata.creationTimestamp --no-headers | tac")
+
+
+def get_last_analysisrun(recipe):
+    return kubectl("get analysisrun --all-namespaces --sort-by=.metadata.creationTimestamp -o jsonpath=\"{.items[0]}\" | jq")
+
+
+def get_last_migrate_logs(recipe, name="7273edf0-eef2-4b21-bb7c-6847e0a37444.test.1-7kq5k"):
+    return kubectl(f"-n kargo-demo logs {name}")
